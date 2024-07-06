2d236f9d78ed35243dc67ff74c66c052082c83fb
Rachana Bandu <rachana.bandu@wissen.com>
2024-07-06 16:59:19 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,11 @@
+const header = React.createElement('parent', {}, [React.createElement('child', {id: 'child1'}, [
+    React.createElement('h1', {id: "heading"}, "I'm an h1 tag"),
+    React.createElement('h2', {}, "I'm an h2 tag")
+]),
+React.createElement('child2', {id: 'child1'}, [
+    React.createElement('h1', {id: "heading"}, "I'm an h1 tag"),
+    React.createElement('h2', {}, "I'm an h2 tag")
+])
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(header);
